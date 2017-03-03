ff1ca7cb640e314a2986e7150402b8defba78db3
Thomas Boerger <thomas@webhippie.de>
2017-03-03 20:47:42 +0100

    Refined store interface for latest requirements

--- store/store.go
+++ store/store.go
@@ -1,14 +1,19 @@
 package store
 
 import (
+	"fmt"
 	"github.com/tboerger/redirects/model"
 	// "golang.org/x/crypto/acme/autocert"
+)
+
+var (
+	ErrRedirectNotFound = fmt.Errorf("Failed to find a redirect")
 )
 
 // Store implements all required data-layer functions for Redirects.
 type Store interface {
 	// GetRedirects retrieves all redirects from the store.
-	GetRedirects() (*model.Redirects, error)
+	GetRedirects() ([]*model.Redirect, error)
 
 	// CreateRedirect creates a redirect on the store.
 	CreateRedirect(*model.Redirect) error
@@ -20,7 +25,7 @@ type Store interface {
 	DeleteRedirect(*model.Redirect) error
 
 	// GetRedirect retrieves a specific redirect from the store.
-	GetRedirect(string) (*model.Redirect, error)
+	GetRedirect(int) (*model.Redirect, error)
 
 	// CertCache returns the cert cache implementation.
 	// CertCache() autocert.Cache
