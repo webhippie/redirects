2e5740b21bd825bfd59690cfff4b3029d54e0d92
Thomas Boerger <thomas@webhippie.de>
2017-03-03 22:35:07 +0100

    Implemented create sub-command

--- cmd/create.go
+++ cmd/create.go
@@ -1,17 +1,50 @@
 package cmd
 
 import (
-	"github.com/Sirupsen/logrus"
+	"fmt"
+	"github.com/tboerger/redirects/model"
+	"github.com/tboerger/redirects/store"
 	"github.com/urfave/cli"
 )
 
 // Create provides the sub-command to create redirect patterns.
 func Create() cli.Command {
 	return cli.Command{
-		Name:  "create",
-		Usage: "Create a redirect pattern",
-		Action: func(c *cli.Context) {
-			logrus.Info("Create")
+		Name:      "create",
+		Usage:     "Create a redirect pattern",
+		ArgsUsage: " ",
+		Flags: []cli.Flag{
+			cli.StringFlag{
+				Name:  "source",
+				Value: "",
+				Usage: "Source for the redirect",
+			},
+			cli.StringFlag{
+				Name:  "destination",
+				Value: "",
+				Usage: "Destination for the redirect",
+			},
+		},
+		Action: func(c *cli.Context) error {
+			return Handle(c, create)
 		},
 	}
+}
+
+func create(c *cli.Context, s store.Store) error {
+	record := &model.Redirect{}
+
+	if val := c.String("source"); c.IsSet("source") && val != "" {
+		record.Source = val
+	} else {
+		return fmt.Errorf("You must provide a source")
+	}
+
+	if val := c.String("destination"); c.IsSet("destination") && val != "" {
+		record.Destination = val
+	} else {
+		return fmt.Errorf("You must provide a destination")
+	}
+
+	return s.CreateRedirect(record)
 }
