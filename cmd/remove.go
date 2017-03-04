8b74536fa12b7836b53b664114b508b5f0930c1d
Thomas Boerger <thomas@webhippie.de>
2017-03-04 13:41:07 +0100

    Fixed storage interface and implemented show command

--- cmd/remove.go
+++ cmd/remove.go
@@ -1,17 +1,34 @@
 package cmd
 
 import (
-	"github.com/Sirupsen/logrus"
+	"fmt"
+	"github.com/tboerger/redirects/store"
 	"github.com/urfave/cli"
+	"strconv"
 )
 
 // Remove provides the sub-command to reove redirect patterns.
 func Remove() cli.Command {
 	return cli.Command{
-		Name:  "remove",
-		Usage: "Remove a redirect pattern",
-		Action: func(c *cli.Context) {
-			logrus.Info("Remove")
+		Name:      "remove",
+		Usage:     "Remove a redirect pattern",
+		ArgsUsage: "<id>",
+		Action: func(c *cli.Context) error {
+			return Handle(c, handleRemove)
 		},
 	}
+}
+
+func handleRemove(c *cli.Context, s store.Store) error {
+	if c.NArg() != 1 {
+		return cli.ShowSubcommandHelp(c)
+	}
+
+	id, err := strconv.Atoi(c.Args().First())
+
+	if err != nil {
+		return fmt.Errorf("Failed to parse the ID")
+	}
+
+	return s.DeleteRedirect(id)
 }
