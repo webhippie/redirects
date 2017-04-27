73f6f528f432a445fb3d982cb424cab58286480b
Thomas Boerger <thomas@webhippie.de>
2017-04-27 10:04:23 +0200

    Extended available helpers

--- router/handler.go
+++ router/handler.go
@@ -89,11 +89,45 @@ func handler(c *gin.Context) {
 
 func helpers() template.FuncMap {
 	return template.FuncMap{
-		"split":    strings.Split,
-		"join":     strings.Join,
-		"toUpper":  strings.ToUpper,
-		"toLower":  strings.ToLower,
-		"contains": strings.Contains,
-		"replace":  strings.Replace,
+		"toUpper":     strings.ToUpper,
+		"toLower":     strings.ToLower,
+		"contains":    contains,
+		"replace":     replace,
+		"split":       split,
+		"join":        join,
+		"firstString": firstString,
+		"lastString":  lastString,
 	}
+}
+
+func replace(s1, s2, s3 string) string {
+	return strings.Replace(s3, s1, s2, -1)
+}
+
+func contains(s1, s2 string) bool {
+	return strings.Contains(s2, s1)
+}
+
+func split(s1, s2 string) []string {
+	return strings.Split(s2, s1)
+}
+
+func join(s1, s2 string) []string {
+	return strings.Join(s2, s1)
+}
+
+func firstString(s1 []string) string {
+	if len(s1) < 1 {
+		return ""
+	}
+
+	return s1[0]
+}
+
+func lastString(s1 []string) string {
+	if len(s1) < 1 {
+		return ""
+	}
+
+	return s1[len(s1)-1]
 }
